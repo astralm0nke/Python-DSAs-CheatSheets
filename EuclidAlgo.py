0e989bb9bb7d392cd5926be409fbba9601cf423b
Finn <142452994+astralm0nke@users.noreply.github.com>
2024-04-09 19:13:41 -0400

    Update EuclidAlgo.py

--- EuclidAlgo.py
+++ EuclidAlgo.py
@@ -1,9 +1,9 @@
 ## Euclid's Algorithm for GCD of 2 numbers in Python
 ## Good info: https://www.csuohio.edu/sites/default/files/85-%202015.pdf
 
-#Idea is if u replace larger number by the remainder of larger/smaller, GCD stays the same
-#So the algorithm just does this recursively until remainder is 0
-#larger = lrg/sml * smaller + remainder
+# Idea is if u replace larger number by the remainder of larger/smaller, GCD stays the same
+# So the algorithm just does this recursively until remainder is 0
+# larger = lrg/sml * smaller + remainder
 
 A = abs(int(input("Specify a: ")))
 B = abs(int(input('Specify b: ')))
